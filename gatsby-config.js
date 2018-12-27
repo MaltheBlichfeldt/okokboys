33cc52e8e5f2b58ca5e90a1cd598e75a841ec303
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2018-12-28 00:18:35 +0100

    slet config ting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,4 @@
 module.exports = {
 
-plugins: [
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      path: `${__dirname}/path/to/markdown/files`,
-      name: "markdown-pages",
-    },
-  },
-  `gatsby-plugin-netlify-cms`,
-  `gatsby-transformer-remark`,
-]
+plugins: [`gatsby-plugin-netlify-cms`],
 }
