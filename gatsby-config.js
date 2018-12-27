5650a1f166f823ee8be044b93b1fe3ebe779ef5d
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2018-12-27 23:42:38 +0100

    pluginds

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+
+plugins: [
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/path/to/markdown/files`,
+      name: "markdown-pages",
+    },
+  },
+  `gatsby-plugin-netlify-cms`,
+  `gatsby-transformer-remark`,
+]
 }
