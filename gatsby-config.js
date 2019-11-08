66e555baffc0179a11cb928341a803394c4fecaf
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2019-11-08 15:34:32 +0100

    gatsby-plugin-netlify-cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     'gatsby-transformer-remark',
+    'gatsby-plugin-netlify-cache',
 
     {
       resolve: "gatsby-remark-embed-spotify",
