9226e43d3c6188f38ea599ccd968f0ae3a4473c4
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2019-11-08 15:07:37 +0100

    nye to episoder

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,22 +46,44 @@ return (
       {/* EPISODE feed */}
 
       <div className="row episode sectionpadding">
-       <div className="col-xs-12 col-sm-4">
-       <iframe src="https://open.spotify.com/embed-podcast/episode/0rjyDs22kVCJcPZOBDeYys" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-      </div>
-      <div className="col-xs-12 col-sm-8 episodetextpadding">
-        <h3>Gilli og Thom Yorke</h3>
+        <div className="col-xs-12 col-sm-4">
+          <iframe src="https://open.spotify.com/embed-podcast/episode/5huIe6YjWs2VymBRhwWWvA" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>      
+        </div>
+        <div className="col-xs-12 col-sm-8 episodetextpadding">
+          <h3>Ganger og Alex Cameron</h3>
 
-        <p>Hvad var årets koncert på Roskilde Festival? Hvordan fungerer Gilli på et helt album? Lyder Thom Yorke som en drøm man glemmer? Og hvad er det perfekte soundtrack til grillfester og italiensk madlavning?
-        <br></br><br></br>
-        Alt dette, og meget mere i dette afsnit af OK OK Boys, hvor vi anmelder Gillis ‘Kiko’, Thom Yorkes ‘ANIMA’, kommer med en masse anbefalinger og introducerer et helt nyt koncept i programmet.
-        <br></br><br></br>
-        <i className="credits">I studiet er Magnus Krog, Lars Andersen og Oliver Aabo.
-        <br></br>
-        Jingle af Anders Belling.</i>
-        </p>
-      </div>
+          <p>Har Ganger lavet et af de mest spændende danske albums i lang tid? Hvor sjove er Alex Camerons tekster egentlig? Er "far rock" fedt? Og kan man stadig bruge en iPod i 2019? 
+          I dette afsnit af OK OK Boys tager vi Gangers 'Mørk' og Alex Camerons 'Miamy Memory' under kærlig behandling. 
+          <br></br><br></br>
+          Derudover anbefaler panelet bl.a. vokalimprovisationer, hård hård hård punk og hele tre slags hip hop. Og vi finder ud af om far rock, CD'er og Oasis er OK eller ikke OK. 
+          <br></br><br></br>
+          <i className="credits">I studiet er Magnus Krog, Lars Andersen og Oliver Aabo.
+          <br></br>
+          Jingle af Anders Belling.</i>
+          </p>
+        </div>
     </div>
+
+      <div className="row episode sectionpadding">
+        <div className="col-xs-12 col-sm-4">
+          <iframe src="https://open.spotify.com/embed-podcast/episode/0rjyDs22kVCJcPZOBDeYys" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        </div>
+        <div className="col-xs-12 col-sm-8 episodetextpadding">
+          <h3>Brockhampton og JÆRV</h3>
+
+          <p>Har Brockhampton lavet et af nyere tids bedste hip-hop albums? Kan man blande The Knife og Rocazino? Og hvordan blev OK OK Boys-jinglen egentlig til? 
+          <br></br>
+          Få svaret på det hele i dette afsnit af OK OK Boys, hvor vi anmelder Brockhamptons 'GINGER' og JÆRVs 'Kriger'. 
+          <br></br>
+          <br></br>
+          Derudover får du som altid stærke anbefalinger fra playlisten og endnu en omgang 'OK eller ikke OK?'.
+          <br></br><br></br>
+          <i className="credits">I studiet er Magnus Krog, Lars Andersen og Oliver Aabo.
+          <br></br>
+          Jingle af Anders Belling.</i>
+          </p>
+        </div>
+      </div>
 
       <section>
         <div className="row episode sectionpadding">
