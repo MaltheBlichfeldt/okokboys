a127e3f4941e482a583c4a43c4208c672800574a
Malthe Blichfeldt <maltheblichfeldt@gmail.com>
2020-01-31 16:08:07 +0100

    De bedste albums

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,6 +45,23 @@ return (
 
       {/* EPISODE feed */}
 
+
+      <div className="row episode sectionpadding">
+      <div className="col-xs-12 col-sm-4">
+      <iframe src="https://open.spotify.com/embed-podcast/episode/27dlYW7ABxxdeamtjZzzbh" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+     </div>
+     <div className="col-xs-12 col-sm-8 episodetextpadding">
+       <h3>De bedste albums</h3>
+       <p>Hvad var de bedste albums i 2019? Det får du svaret på i dette afsnit, hvor vi giver dig de bedste danske plader og det bedste fra udlandet - og der er alt fra afrikanske energibomber, over fantastiske historiefortællere til dansk-russiske technobangers. </p>
+          <p>Og så er der en omgang OK eller IKKE OK i en speciel 2019-udgave.</p>
+
+          <i className="credits">
+            I studiet er Magnus Krog, Lars Andersen og Oliver Aabo.
+            Jingle af Anders Belling.
+          </i>
+     </div>
+   </div>
+
       <div className="row episode sectionpadding">
         <div className="col-xs-12 col-sm-4">
           <iframe src="https://open.spotify.com/embed-podcast/episode/5huIe6YjWs2VymBRhwWWvA" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>      
@@ -62,7 +79,7 @@ return (
           Jingle af Anders Belling.</i>
           </p>
         </div>
-    </div>
+      </div>
 
       <div className="row episode sectionpadding">
         <div className="col-xs-12 col-sm-4">
@@ -85,62 +102,8 @@ return (
         </div>
       </div>
 
-      <section>
-        <div className="row episode sectionpadding">
-         <div className="col-xs-12 col-sm-4">
-         <iframe src="https://open.spotify.com/embed-podcast/episode/6kV0Tc63ARdtemzRSRkPwx" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        </div>
-        <div className="col-xs-12 col-sm-8 episodetextpadding">
-          <h3>Gilli og Thom Yorke</h3>
 
-          <p>Hvad var årets koncert på Roskilde Festival? Hvordan fungerer Gilli på et helt album? Lyder Thom Yorke som en drøm man glemmer? Og hvad er det perfekte soundtrack til grillfester og italiensk madlavning?
-          <br></br><br></br>
-          Alt dette, og meget mere i dette afsnit af OK OK Boys, hvor vi anmelder Gillis ‘Kiko’, Thom Yorkes ‘ANIMA’, kommer med en masse anbefalinger og introducerer et helt nyt koncept i programmet.
-          <br></br><br></br>
-          <i className="credits">I studiet er Magnus Krog, Lars Andersen og Oliver Aabo.
-          <br></br>
-          Jingle af Anders Belling.</i>
-          </p>
-        </div>
-      </div>
-      
-{/*
-        <div className="row episode sectionpadding">
-          <div className="col-xs-12 col-sm-4">
-            <iframe title="Roskilde Festival-special" src="https://open.spotify.com/embed-podcast/episode/6CqEXGHxS0UyoXNLGalFoj" width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-          </div>
-          <div className="col-xs-12 col-sm-8 episodetextpadding">
-          <h3>Roskilde Festival-special</h3>
 
-            <p>Hvad skal man se på Roskilde Festival 2019? Det fortæller panelet i denne specialudgave af OK OK Boys, der er dedikeret til årets festival.<br></br>
-
-            Vi har dykket ned i de 188 navne på plakaten, og fundet det bedste til jer. Der bliver anbefalet moshpit-venlig hip-hop, vild afro-jazz, god gedigen rock og alt muligt andet.
-            <br></br><br></br>
-            <i className="credits">I studiet er Magnus Krog, Lars Andersen og Oliver Aabo.
-            <br></br>
-            Jingle af Anders Belling.</i>
-            </p>
-        </div>
-        </div>
-
-        <div className="row sectionpadding episode">
-          <div className="col-xs-12 col-sm-4">
-            <iframe title="Album covers, Show Me the Body og Jenny Wilson" src="https://open.spotify.com/embed-podcast/episode/7n6uhlsydEVN1kcmTi9tey" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>        </div>
-          <div className="col-xs-12 col-sm-8 episodetextpadding">
-            <h3>Album covers, Show Me the Body og Jenny Wilson</h3>
-            <p>Kan man lave spoken word punk? Har Anderson .Paak kun lavet grimme album covers? Og hvem af drengene var sulten, da de valgte numre til playlisten?
-            <br></br> <br></br>
-            Det bliver diskuteret i dette afsnit af OK OK Boys, hvor vi anmelder New Yorker-punkerne Show Me The Bodys album ‘Dog Whistle’ og Jenny Wilson meget personlige album ‘TRAUMA’ om et seksuelt overgreb. Og så snakker vi de bedste og værste album covers.
-            <br></br> <br></br>
-            <i className="credits">I studiet er Magnus Krog, Lars Andersen og Oliver Aabo.
-            <br></br>
-            Jingle af Anders Belling.</i>
-            </p>
-          </div>
-        </div>
-*/}
-
-</section>
 
     <section className="redaktionen">
       <div className="row">
